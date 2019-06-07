508335507ca3c7697d06e7caa4e01170ca8b4732
Ronaldo Cano <rcano@outlook.es>
2019-06-06 20:26:23 -0600

    model forms project

--- Level 3/forms_project/forms_app/forms.py
+++ Level 3/forms_project/forms_app/forms.py
@@ -1,6 +1,33 @@
 from django import forms
+from django.core import validators
+
+#def check_for_z(value):
+#    if value[0].lower() != 'z':
+#        raise forms.ValidationError("NAME NEEDS TO START WITH Z")
 
 class FormName(forms.Form):
+    #name = forms.CharField(validators = [check_for_z])
     name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField()
     text = forms.CharField(widget = forms.Textarea)
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("Make Sure emails Match!")
+    #botcatcher = forms.CharField(required = False,widget=forms.HiddenInput)
+    #botcatcher = forms.CharField(required = False,widget=forms.HiddenInput,
+    #                            validators=[validators.MaxLengthValidator(0)])
+
+
+    #def clean_botcatcher(self):
+    #    botcatcher = self.cleaned_data['botcatcher']
+    #    if len(botcatcher) > 0:
+    #        raise forms.ValidationError("Gotcha Bot!")
+    #    return botcatcher
+
+
--- django stuff/second_project/second_project/second_app/views.py
+++ django stuff/second_project/second_project/second_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from second_app.models import User
+from second_app.forms import NewUserForm
+#from second_app.models import User
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("<em>Hello World 2</em>")
+    #return HttpResponse("<em>Hello World 2</em>")
+    return render(request,'second_app/index.html')
 
 def help(request):
     #return HttpResponse("<em>Hello World 2</em>")
@@ -13,11 +15,24 @@ def help(request):
 
     return render(request,'second_app/help.html',context =my_dict)
 
-def users(request):
+#def users(request):
     #return HttpResponse("<em>Hello World 2</em>")
     #my_dict = {'users':"Users page"}
-    user_list = User.objects.order_by('FirstName')
-    user_dict = {'users':user_list}
+ #   user_list = User.objects.order_by('FirstName')
+ #   user_dict = {'users':user_list}
     
 
-    return render(request,'second_app/users.html',context =user_dict)
+  #  return render(request,'second_app/users.html',context =user_dict)
+
+def users(request):
+    form = NewUserForm()
+
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit = True)
+            return index(request)
+        else:
+            print('ERROR FORM INVALID')
+    return render(request,'second_app/users.html',{'form':form})
