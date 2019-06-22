7c6891fa7dfed5bcfc7ac729299270a7902babe8
Ronaldo Cano <rcano@outlook.es>
2019-06-22 08:48:03 -0600

    relative urls and base template

--- Level 4/learning_templates/learning_templates/settings.py
+++ Level 4/learning_templates/learning_templates/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATES_DIR = OS.PATH.join(BASE_DIR,'Templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR,'Templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
