f802777634885722db2fee6c36d4c897733dd7bc
rcano@mrpfd.com <rcano@mrpfd.com>
2019-08-15 21:37:55 -0600

    correction to comment on post redirect link

--- blog_project/mysite/mysite/urls.py
+++ blog_project/mysite/mysite/urls.py
@@ -22,6 +22,6 @@ from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', include('blog.urls')),
-     path('login/', LoginView.as_view(template_name='registration/login.html'), name="login"),
-     path('logout/', LogoutView.as_view(template_name='registration/login.html'), name="logout"),
+     path('accounts/login/', LoginView.as_view(template_name='registration/login.html'), name="login"),
+     path('accounts/logout/', LogoutView.as_view(template_name='registration/login.html'), name="logout"),
 ]
